02ee25fd7d2c40627b741e723d682facbf8be08c
raiesbo <raiesbo@gmail.com>
2021-11-11 08:57:39 +0100

    finish categories functionality

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const burger = document.querySelector('.burger');
 const burgerWrapper = document.querySelector('.burgerWrapper');
 const nav = document.querySelector('.navigation');
 const main = document.querySelector('.mainContent');
+const showMoreButton = document.querySelector('.showMore');
+const showMoreMessage = document.querySelector('.showMoreMessage');
 
 
 // Handlers
@@ -12,5 +14,18 @@ const burgerClickHandler = () => {
     burgerWrapper.classList.toggle('burgerWrapperActive')
 }
 
+const showMoreHandler = (e) => {
+    const btn = e.target;
+    
+    if (btn.textContent === 'Ver más') {
+        showMoreButton.textContent = "Ver menos"
+        showMoreMessage.innerText = "Ups, parece que no hay más contenido.";
+    } else {
+        showMoreButton.textContent = "Ver más"
+        showMoreMessage.innerText = "";
+    }
+}
+
 // Event listeners
 burger.addEventListener('click', burgerClickHandler);
+showMoreButton.addEventListener('click', showMoreHandler)
